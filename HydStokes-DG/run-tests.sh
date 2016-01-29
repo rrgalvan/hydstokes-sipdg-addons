a875fbc45f226d31fa47f9c61a2abf57f18f60a4
Rafa <rafael.rodriguez@uca.es>
2016-01-29 20:36:35 +0100

    Eliminanted redundant testing of FreeFem++ installation

--- HydStokes-DG/run-tests.sh
+++ HydStokes-DG/run-tests.sh
@@ -3,6 +3,3 @@
 ## Simple automated test suite in the bash command. It is based on
 ## bats <https://github.com/sstephenson/bats>.
 
-@test "Check that the FreeFem++ interpreter is available" {
-    command -v FreeFem++-nw
-}
